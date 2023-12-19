cb9eb69e4445116a4478c99cda9374fdd33da4b6
Zaid Ansari <zaid.ansari@nonstopio.com>
2023-12-19 19:11:09 +0530

    Fixed Text over flow of country names

--- lib/src/presentation/widgets/country_dropdown_widget.dart
+++ lib/src/presentation/widgets/country_dropdown_widget.dart
@@ -14,22 +14,29 @@ class CountryDropDownWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        FontTextWidget(text: country.flag),
-        const SizedBox(width: 8),
-        Text(
-          '${country.iso2Code.toUpperCase()} +${country.dialCode}',
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-          ),
+        Row(
+          children: [
+            FontTextWidget(text: country.flag),
+            const SizedBox(width: 8),
+            Text(
+              '${country.iso2Code.toUpperCase()} +${country.dialCode}',
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(width: 8),
+          ],
         ),
-        const Spacer(),
-        Text(
-          country.countryName,
-          style: const TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w500,
+        Flexible(
+          child: Text(
+            country.countryName,
+            style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                overflow: TextOverflow.ellipsis),
           ),
         ),
       ],
