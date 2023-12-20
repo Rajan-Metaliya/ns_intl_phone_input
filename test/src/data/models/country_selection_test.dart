6c07e40c39dea1a86b73685b8b9822531d219b94
Zaid Ansari <zaid.ansari@nonstopio.com>
2023-12-20 18:42:56 +0530

    Added test cases to check formatting of numbers

--- test/src/data/models/country_selection_test.dart
+++ test/src/data/models/country_selection_test.dart
@@ -12,6 +12,7 @@ void main() {
           regions: {'Asia'},
           iso2Code: 'in',
           intlDialCode: '91',
+          format: '.....-.....',
         );
 
         final country2 = CountryModel(
@@ -23,17 +24,36 @@ void main() {
 
         final selection1 = CountrySelection(
           selectedCountry: country1,
-          formattedPhoneNumber: '+91 1234567890',
+          formattedPhoneNumber: '12345-67890',
           unformattedPhoneNumber: '1234567890',
         );
 
         final selection2 = CountrySelection(
           selectedCountry: country2,
-          formattedPhoneNumber: '+1 9876543210',
+          formattedPhoneNumber: '9876543210',
           unformattedPhoneNumber: '9876543210',
         );
 
         expect(selection1, isNot(equals(selection2)));
+      });
+
+      test('Test the complete Formatted & UnFormatted Numbers', () {
+        final country = CountryModel(
+          countryName: 'India',
+          regions: {"asia"},
+          iso2Code: 'in',
+          intlDialCode: '91',
+          flag: '🇮🇳',
+          format: '.....-.....',
+        );
+
+        final selection = CountrySelection(
+            selectedCountry: country,
+            formattedPhoneNumber: '98778-78998',
+            unformattedPhoneNumber: '9877878998');
+
+        expect(selection.completePhoneNumber, '91 9877878998');
+        expect(selection.completeFormattedPhoneNumber, '91 98778-78998');
       });
     },
   );
